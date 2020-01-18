1ad989cac35b21860c709a92b464eba11ca2afa4
Rizwan Haidar <47399482+rizwanhaidar@users.noreply.github.com>
2020-01-18 19:01:48 +0500

    Update Script.R

--- Script.R
+++ Script.R
@@ -243,3 +243,21 @@ factor_temperature_vector <- factor(temperature_vector, order = TRUE, levels = c
 factor_temperature_vector
 
 
+# Code to build factor_survey_vector
+survey_vector <- c("M", "F", "F", "M", "M")
+factor_survey_vector <- factor(survey_vector)
+
+# Specify the levels of factor_survey_vector
+levels(factor_survey_vector) <- c("Female", "Male")
+
+# Build factor_survey_vector with clean levels
+survey_vector <- c("M", "F", "F", "M", "M")
+factor_survey_vector <- factor(survey_vector)
+levels(factor_survey_vector) <- c("Female", "Male")
+factor_survey_vector
+
+# Generate summary for survey_vector
+summary(survey_vector)
+
+# Generate summary for factor_survey_vector
+summary(factor_survey_vector)
