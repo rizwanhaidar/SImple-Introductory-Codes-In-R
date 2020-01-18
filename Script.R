1b0587e101a7624a6ecfabe58560e7bb63bea727
Rizwan Haidar <47399482+rizwanhaidar@users.noreply.github.com>
2020-01-18 18:42:21 +0500

    Update Script.R
    Factors in R

--- Script.R
+++ Script.R
@@ -222,3 +222,24 @@ us_visitors <- visitors[,1]
 mean(us_visitors)
 
 #Chapter 3 ends up here
+
+# Sex vector
+sex_vector <- c("Male", "Female", "Female", "Male", "Male")
+
+# Convert sex_vector to a factor
+factor_sex_vector <- factor(sex_vector)
+
+# Print out factor_sex_vector
+factor_sex_vector
+
+# Animals
+animals_vector <- c("Elephant", "Giraffe", "Donkey", "Horse")
+factor_animals_vector <- factor(animals_vector)
+factor_animals_vector
+
+# Temperature
+temperature_vector <- c("High", "Low", "High","Low", "Medium")
+factor_temperature_vector <- factor(temperature_vector, order = TRUE, levels = c("Low", "Medium", "High"))
+factor_temperature_vector
+
+
