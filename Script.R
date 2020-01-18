5f8a4e4f5d85a31d707dc1f61dbd26f4d5ae21e8
Rizwan Haidar <47399482+rizwanhaidar@users.noreply.github.com>
2020-01-18 19:10:28 +0500

    Update Script.R

--- Script.R
+++ Script.R
@@ -261,3 +261,17 @@ summary(survey_vector)
 
 # Generate summary for factor_survey_vector
 summary(factor_survey_vector)
+
+# Build factor_survey_vector with clean levels
+survey_vector <- c("M", "F", "F", "M", "M")
+factor_survey_vector <- factor(survey_vector)
+levels(factor_survey_vector) <- c("Female", "Male")
+
+# Male
+male <- factor_survey_vector[1]
+
+# Female
+female <- factor_survey_vector[2]
+
+# Battle of the sexes: Male 'larger' than female?
+male > female
